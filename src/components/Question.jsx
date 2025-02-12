486403c18f425fa07e64f017864051a968ca0bb3
Tailung1 <ramazsamitashvili11@gmail.com>
2025-02-12 18:55:23 +0400

    change layout of code to make code more readable

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -5,9 +5,15 @@ export default function Question(props){
     return (
         <div className='question-div'>
         <p className='question'
-        onClick={()=>props.active === props.questionNumber?
-        props.setActive(0): props.setActive(props.questionNumber)}>Question {props.questionNumber}</p>
-        {props.active === props.questionNumber?<p className='answer'>Answer {props.questionNumber}</p> : null}
+        onClick={()=>
+            props.active === props.questionNumber
+            ? props.setActive(0)
+            : props.setActive(props.questionNumber)}
+            >Question {props.questionNumber}</p>
+
+            {props.active === props.questionNumber
+            ?<p className='answer'>Answer {props.questionNumber}</p>
+            : null}
         </div>
     )
 }
